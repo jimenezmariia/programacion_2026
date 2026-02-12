fd45b41322264e1ae382e80950eeb3527faa1926
María Jiménez <a25216375@alumnos.uady.mx>
2026-02-12 08:50:03 -0600

    Add Celular class definition in Celular.h

---
+++ examen_01/Celular.h
@@ -0,0 +1,42 @@
+### Archivo Celular.h
+´´´cpp
+#ifndef CELULAR_H
+#define CELULAR_H
+
+#include <string>
+
+class Celular {
+private:
+  std::string marca;
+  std::string modelo;
+  int almacenamiento;
+  int ram;
+  double precio;
+
+public:
+  Celular();
+  Celular (std::string marca, std::string modelo, int almacenamiento, int ram, double precio);
+
+  void actualizarMarca(std::string nuevaMarca);
+  void actualizarModelo(std::string nuevoModelo);
+  void actualizarAlmacenamiento(int nuevoAlmacenamiento)
+  void actualizarRam(int nuevaRam);
+  void actualizarPrecio(double nuevoPrecio);
+
+  std::string obtenerMarca();
+  std::string obtenerModelo();
+  int obtenerAlmacenamiento();
+  int obtenerRam();
+  double obtenerPrecio();
+
+  void mostrarMarca();
+  void mostrarCodigo();
+  void mostrarAlmacenamiento();
+  void mostrarRam();
+  void mostrarPrecio();
+  void mostrarInformacionCompleta();
+
+  void incrementarPrecio();
+    
+};
+#endif
